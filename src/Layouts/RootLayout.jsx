097951d749d25d4d7e4dd0697131d79e5985c784
ishoef <ismailhossennayeb@gmail.com>
2025-06-07 11:57:08 +0300

    Enhance Footer component with social links and navigation, and update RootLayout to include Footer

--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 import Header from '../Components/Header/Header';
+import Footer from '../Components/Footer/Footer';
 
 export const responsive = "w-11/12 md:w-10/12 lg:w-9/12 mx-auto"
 const RootLayout = () => {
 
     return (
-        <div>
-            <Header></Header>
-            <Outlet></Outlet>
+      <div>
+        <Header></Header>
+        <div className='min-h-[calc(100vh-360px)]'>
+          <Outlet></Outlet>
         </div>
+        <Footer></Footer>
+      </div>
     );
 };
 
