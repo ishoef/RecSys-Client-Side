352a9df5996e596e94b04ef7a4e40f2d0f598b85
ishoef <ismailhossennayeb@gmail.com>
2025-06-16 01:04:11 +0300

    Refactor WhatYouGet component; enhance layout and styling for benefits and stats sections; improve user interface and experience.

--- src/Pages/Home/WhatYouGet/WhatYouGet.jsx
+++ src/Pages/Home/WhatYouGet/WhatYouGet.jsx
@@ -1,35 +1,107 @@
-import React, { use } from 'react';
-import { responsive } from '../../../Layouts/RootLayout';
-import Button from '../../../Components/Button/Button';
-import Title from '../../../Components/Title/Title';
-import { AuthContext } from '../../../Context/AuthProvider';
+import React, { use } from "react";
+import { responsive } from "../../../Layouts/RootLayout";
+import Button from "../../../Components/Button/Button";
+import Title from "../../../Components/Title/Title";
+import { AuthContext } from "../../../Context/AuthProvider";
+import { SiTicktick } from "react-icons/si";
+import { FaRegClock, FaRegStar } from "react-icons/fa";
+import { GiArcheryTarget } from "react-icons/gi";
+import { GoLightBulb } from "react-icons/go";
+import { TbUsers } from "react-icons/tb";
+import { PiUsersBold } from "react-icons/pi";
+import { LiaCertificateSolid } from "react-icons/lia";
+import { IoStatsChart } from "react-icons/io5";
 
 const WhatYouGet = () => {
+  const { user } = use(AuthContext);
+  const benefits = [
+    {
+      icon: <SiTicktick />,
+      title: "Save money on better alternatives",
+    },
+    {
+      icon: <FaRegClock />,
+      title: "Save time with instant recommendations",
+    },
+    {
+      icon: <GiArcheryTarget />,
+      title: "Find products that match your values",
+    },
+    {
+      icon: <GoLightBulb />,
+      title: "Discover innovative solutions",
+    },
+    {
+      icon: <FaRegStar />,
+      title: "Access to exclusive deals and offers",
+    },
+    {
+      icon: <TbUsers />,
+      title: "Connect with like-minded consumers",
+    },
+  ];
 
-    const { user } = use(AuthContext);
+  const stats = [
+    {
+      icon: <PiUsersBold />,
+      value: "5K+",
+      label: "Active Users",
+    },
+    {
+      icon: <FaRegStar />,
+      value: "30K+",
+      label: "Recommendations",
+    },
+    {
+      icon: <IoStatsChart />,
+      value: "94%",
+      label: "Success Rate",
+    },
+    {
+      icon: <LiaCertificateSolid />,
+      value: "$2M+",
+      label: "Money Saved",
+    },
+  ];
 
-    return (
-      <div className={`${responsive} my-10`}>
-        <div className="bg-primary w-full  rounded-2xl p-10 flex items-center gap-10">
-          <div className="basis-1/2 border border-white flex flex-col items-start gap-5">
-            <Title title={"What You Get With RecoSyS"} className={"text-white"} />
-            <p className='poppins text-white'>
-              More than just product recommendations - join a community that's
-              changing how people discover and schoose products.
-            </p>
+  return (
+    <div className={`${responsive} my-20`}>
+      <div className="bg-primary w-full  rounded-2xl p-16 flex items-center gap-10">
+        <div className="basis-1/2 flex flex-col items-start gap-5">
+          <Title title={"What You Get With RecoSyS"} className={"text-white"} />
+          <p className="poppins text-white">
+            More than just product recommendations - join a community that's
+            changing how people discover and schoose products.
+          </p>
 
-            <div></div>
-            <Button to={user ? "/queries" : "/auth/register"} text={"Join Our Community"} className={"bg-white btn rounded-3xl poppins text-primary"} />
+          <div className="grid grid-cols-2 gap-5 w-full">
+            {benefits.map((benefit) => (
+              <div className=" bg-green-200/20 flex items-center gap-3 p-4 text-white poppins rounded-2xl">
+                <p className="text-2xl">{benefit.icon}</p>
+                <p>{benefit.title}</p>
+              </div>
+            ))}
           </div>
-          <div className="basis-1/2 grid grid-cols-2 border gap-5">
-            <div className='border'>Active Users</div>
-            <div>Active Users</div>
-            <div>Active Users</div>
-            <div>Active Users</div>
+          <Button
+            to={user ? "/queries" : "/auth/register"}
+            text={"Join Our Community"}
+            className={"bg-white btn rounded-3xl poppins text-primary"}
+          />
+        </div>
+        <div className=" w-full basis-1/2 ">
+          <div className="grid grid-cols-2 gap-5">
+            {stats.map((stat) => (
+              <div className="flex flex-col items-center gap-3 p-5 text-white  bg-green-200/20 rounded-2xl">
+                <p className="text-3xl">{stat.icon}</p>
+                <p className="text-3xl poppins font-semibold">{stat.value}</p>
+                <p className="poppins">{stat.label}</p>
+              </div>
+            ))}
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default WhatYouGet;
